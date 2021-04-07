23bea0b9ce850e4e071a876ee3f01ccfbb8c11ab
Delta Lyczak <lyczakg@lafayette.edu>
2021-04-06 20:13:45 -0400

    Add server startup logic, binding to DefaultController

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+#include <memory>
+#include <restbed>
+
+#include "defaultcontroller.h"
 
 using namespace std;
+using namespace restbed;
 
-int main()
+int main(const int, const char**)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    shared_ptr<DefaultController> ctlDefault = make_shared<DefaultController>();
+    const shared_ptr<Resource> resDefault = ctlDefault->getResource();
+
+    auto settings = make_shared<Settings>();
+    settings->set_port(1312);
+    settings->set_default_header("Connection", "close");
+
+    Service service;
+    service.publish(resDefault);
+    service.start(settings);
+
+    return EXIT_SUCCESS;
 }
