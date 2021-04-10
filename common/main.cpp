0cbd72c1343dc7366bcfbb363785d9af72668cb7
Delta Lyczak <lyczakg@lafayette.edu>
2021-04-10 13:09:33 -0400

    Add basic CLI test to common main()

--- common/main.cpp
+++ common/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <QString>
+#include <QDateTime>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+#include "user.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    User * user1 = new User();
+    user1->setUsername("Womp");
+    user1->setPassword("thwompthwomp");
+
+    QJsonObject * userJson1 = new QJsonObject();
+    user1->toJson(* userJson1);
+
+    User * user2 = new User();
+    user2->fromJson(* userJson1);
+
+    QJsonObject * userJson2 = new QJsonObject();
+    user2->toJson(* userJson2);
+
+    QJsonDocument * doc1 = new QJsonDocument(* userJson1);
+    QJsonDocument * doc2 = new QJsonDocument(* userJson2);
+
+    cout << doc1->toJson().toStdString() << endl;
+    cout << doc2->toJson().toStdString() << endl;
+
     return 0;
 }
