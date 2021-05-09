d1d334656ef41d797407ef3389c54c8f3fc3a11a
Delta Lyczak <lyczakg@lafayette.edu>
2021-05-09 13:06:00 -0400

    Fix numerous UsersController errors to allow create/list different UserTypes

--- server/userscontroller.cpp
+++ server/userscontroller.cpp
@@ -21,35 +21,54 @@ void UsersController::getHandler(const shared_ptr<Session> session)
 {
     Db * db = Db::instance();
 
-    QJsonArray parents;
-    QJsonArray shelterOwners;
-    QJsonArray admins;
-    db->foreachParent([&parents](Parent & p) {
-        QJsonObject o;
-        p.toJson(o);
-        parents.append(o);
-    });
+    string uTypeStr = session->
+            get_request()->
+            get_path_parameter(TYPE);
 
-    db->foreachShelterOwner([&](ShelterOwner & so) {
-        QJsonObject o;
-        so.toJson(o);
-        shelterOwners.append(o);
-    });
-
-    db->foreachAdmin([&](Administrator & a) {
-        QJsonObject o;
-        a.toJson(o);
-        admins.append(o);
-    });
+    UserType uType = User::getType(QString::fromStdString(uTypeStr));
 
     BNBResponse res;
-    res.setResult(parents);
+    int statusCode = restbed::OK;
+
+    QJsonArray users;
+    if (uType == PARENT)
+    {
+        db->foreachParent([&](Parent & p) {
+                QJsonObject o;
+                p.toJson(o);
+                users.append(o);
+            });
+        res.setResult(users);
+    }
+    else if (uType == SHELTER_OWNER)
+    {
+        db->foreachShelterOwner([&](ShelterOwner & so) {
+                QJsonObject o;
+                so.toJson(o);
+                users.append(o);
+            });
+        res.setResult(users);
+    }
+    else if (uType == ADMINISTRATOR)
+    {
+        db->foreachAdmin([&](Administrator & a) {
+                QJsonObject o;
+                a.toJson(o);
+                users.append(o);
+            });
+        res.setResult(users);
+    }
+    else
+    {
+        res.setError("Invalid user-type");
+        statusCode = restbed::BAD_REQUEST;
+    }
 
     QJsonObject resObj;
     res.toJson(resObj);
 
     const string resStr = QJsonDocument(resObj).toJson().toStdString();
-    session->close(restbed::OK, resStr, {
+    session->close(statusCode, resStr, {
                        { "Content-Type", "application/json" },
                        { "Content-Length", ::to_string(resStr.length()) },
                        { "Connection", "close" }
@@ -60,8 +79,8 @@ void UsersController::postHandler(const shared_ptr<Session> session)
 {
     Db * db = Db::instance();
 
-    size_t contentLen = session->get_request()->get_header("Content-Length", 0);
-    string modelType = session->get_request()->get_path_parameter(TYPE);
+    size_t contentLen = session->get_request()->
+            get_header("Content-Length", 0);
 
     BNBResponse res;
     int statusCode = restbed::INTERNAL_SERVER_ERROR;
@@ -76,26 +95,31 @@ void UsersController::postHandler(const shared_ptr<Session> session)
         if (reqBody.isObject())
         {
             BNBRequest req;
-            req.setType(QString::fromStdString(modelType));
+
+            string uTypePathStr = session->get_request()->
+                        get_path_parameter(TYPE);
+            req.setType(QString::fromStdString(uTypePathStr));
+
             req.fromJson(reqBody.object());
 
             BNBModel * model = nullptr;
-            QString type = req.getType();
+            UserType uType = User::getType(req.getType());
+
             QDateTime now = QDateTime::currentDateTimeUtc();
-            if (type.startsWith("parent"))
+            if (uType == PARENT)
             {
                 Parent * p = new Parent();
                 p->fromJson(req.getPayload().toObject());
                 p->getUser().setCreatedAt(now);
                 model = p;
             }
-            else if (type.startsWith("shelterowner")){
+            else if (uType == SHELTER_OWNER){
                 ShelterOwner * so = new ShelterOwner();
                 so->fromJson(req.getPayload().toObject());
                 so->getUser().setCreatedAt(now);
                 model = so;
             }
-            else if (type.startsWith("administrator")){
+            else if (uType == ADMINISTRATOR){
                 Administrator * a = new Administrator();
                 a->fromJson(req.getPayload().toObject());
                 a->getUser().setCreatedAt(now);
@@ -103,7 +127,7 @@ void UsersController::postHandler(const shared_ptr<Session> session)
             }
             else
             {
-                res.setError("Invalid type");
+                res.setError("Invalid user-type");
                 statusCode = restbed::BAD_REQUEST;
             }
 
@@ -111,29 +135,31 @@ void UsersController::postHandler(const shared_ptr<Session> session)
             if (model != nullptr)
                 validErr = model->validation();
 
-            if (!validErr.isEmpty())
+            if (model == nullptr)
             {
-                res.setError(validErr);
-                statusCode = restbed::BAD_REQUEST;
+                if (!validErr.isEmpty())
+                {
+                    res.setError(validErr);
+                    statusCode = restbed::BAD_REQUEST;
+                }
             }
             else
             {
                 QString dbErr;
-                if (db->createParentAndUser(* (Parent *) model, &dbErr))
+                if (uType == PARENT)
                 {
-                    QJsonObject mObj;
-                    model->toJson(mObj);
-                    res.setResult(mObj);
-                    statusCode = restbed::CREATED;
+                    db->createParentAndUser(* (Parent *) model, &dbErr);
                 }
-                else if (db->createShelterOwnerAndUser(* (ShelterOwner *) model, &dbErr))
+                else if (uType == SHELTER_OWNER)
                 {
-                    QJsonObject mObj;
-                    model->toJson(mObj);
-                    res.setResult(mObj);
-                    statusCode = restbed::CREATED;
+                    db->createShelterOwnerAndUser(* (ShelterOwner *) model, &dbErr);
                 }
-                else if (db->createAdministratorAndUser(* (Administrator *) model, &dbErr))
+                else if (uType == ADMINISTRATOR)
+                {
+                    db->createAdministratorAndUser(* (Administrator *) model, &dbErr);
+                }
+
+                if (dbErr.isEmpty())
                 {
                     QJsonObject mObj;
                     model->toJson(mObj);
@@ -145,7 +171,6 @@ void UsersController::postHandler(const shared_ptr<Session> session)
                     res.setError(dbErr);
                     statusCode = restbed::INTERNAL_SERVER_ERROR;
                 }
-
             }
 
             if (model != nullptr)
